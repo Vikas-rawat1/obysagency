dc3a9833f1275796a02419b06a04f3b5bb5e3ab2
vikas <rawatvikas689@gmail.com>
2024-07-22 19:41:27 +0530

    commit

--- script.js
+++ script.js
@@ -110,19 +110,15 @@ function cursorAnimation() {
   });
   Shery.makeMagnet("#nav-part2 h4", {});
 }
-locomotiveAnimation();
-// loadingAnimation();
-// cursorAnimation();
-
 function sheryAnimation() {
   Shery.imageEffect(".image-div", {
     style: 5,
     // debug: true,
     config: {
-      a: { value: 2, range: [0, 30] },
-      b: { value: 0.75, range: [-1, 1] },
+      a: { value: 1.83, range: [0, 30] },
+      b: { value: 0.92, range: [-1, 1] },
       zindex: { value: -9996999, range: [-9999999, 9999999] },
-      aspect: { value: 0.7272749998247967 },
+      aspect: { value: 0.7241195605115471 },
       ignoreShapeAspect: { value: true },
       shapePosition: { value: { x: 0, y: 0 } },
       shapeScale: { value: { x: 0.5, y: 0.5 } },
@@ -136,21 +132,24 @@ function sheryAnimation() {
       durationOut: { value: 1, range: [0.1, 5] },
       durationIn: { value: 1.5, range: [0.1, 5] },
       displaceAmount: { value: 0.5 },
-      masker: { value: false },
-      maskVal: { value: 1, range: [1, 5] },
+      masker: { value: true },
+      maskVal: { value: 1.24, range: [1, 5] },
       scrollType: { value: 0 },
       geoVertex: { range: [1, 64], value: 1 },
       noEffectGooey: { value: true },
       onMouse: { value: 0 },
-      noise_speed: { value: 0.84, range: [0, 10] },
-      metaball: { value: 0.38, range: [0, 2] },
-      discard_threshold: { value: 0.5, range: [0, 1] },
-      antialias_threshold: { value: 0, range: [0, 0.1] },
+      noise_speed: { value: 0.69, range: [0, 10] },
+      metaball: { value: 0.34, range: [0, 2] },
+      discard_threshold: { value: 0.69, range: [0, 1] },
+      antialias_threshold: { value: 0.01, range: [0, 0.1] },
       noise_height: { value: 0.5, range: [0, 2] },
-      noise_scale: { value: 7.63, range: [0, 100] },
+      noise_scale: { value: 6.87, range: [0, 100] },
     },
     gooey: true,
   });
 }
 
+locomotiveAnimation();
+loadingAnimation();
+cursorAnimation();
 sheryAnimation();

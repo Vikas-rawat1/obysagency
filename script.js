8ab8e234df210c0b8a319abd59e484cdb57a21e0
vikas <rawatvikas689@gmail.com>
2024-07-24 03:37:31 +0530

    commit

--- script.js
+++ script.js
@@ -225,257 +225,26 @@ function flagAnimation() {
   });
 }
 
-// locomotiveAnimation();
-// loadingAnimation();
-// cursorAnimation();
-// sheryAnimation();
-// flagAnimation();
-
-// gsap.registerPlugin(ScrollTrigger);
-
-// function locomotiveAnimation() {
-//   const locoScroll = new LocomotiveScroll({
-//     el: document.querySelector("#main"),
-//     smooth: true,
-//   });
-
-//   locoScroll.on("scroll", ScrollTrigger.update);
-
-//   ScrollTrigger.scrollerProxy("#main", {
-//     scrollTop(value) {
-//       return arguments.length
-//         ? locoScroll.scrollTo(value, 0, 0)
-//         : locoScroll.scroll.instance.scroll.y;
-//     },
-//     getBoundingClientRect() {
-//       return {
-//         top: 0,
-//         left: 0,
-//         width: window.innerWidth,
-//         height: window.innerHeight,
-//       };
-//     },
-//     pinType: document.querySelector("#main").style.transform
-//       ? "transform"
-//       : "fixed",
-//   });
-
-//   ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-//   ScrollTrigger.refresh();
-
-//   window.addEventListener("resize", () => locoScroll.update());
-// }
-
-// function loadingAnimation() {
-//   var tl = gsap.timeline();
-//   tl.from(".line h1", {
-//     y: 150,
-//     stagger: 0.25,
-//     duration: 0.6,
-//     delay: 0.5,
-//   });
-//   tl.from("#line1-part1", {
-//     opacity: 0,
-//     onStart: function () {
-//       var h5timer = document.querySelector("#line1-part1 h5");
-//       var grow = 0;
-//       setInterval(function () {
-//         if (grow < 100) {
-//           h5timer.innerHTML = grow++;
-//         } else {
-//           h5timer.innerHTML = grow;
-//         }
-//       }, 27);
-//     },
-//   });
-//   tl.to(".line h2", {
-//     animationName: "loaderAnime",
-//     opacity: 1,
-//   });
-//   tl.to("#loader", {
-//     opacity: 0,
-//     duration: 0.2,
-//     delay: 2.6,
-//   });
-//   tl.from("#page1", {
-//     delay: 0.1,
-//     y: 1600,
-//     duration: 0.5,
-//     ease: Power4,
-//   });
-//   tl.to("#loader", {
-//     display: "none",
-//   });
-//   tl.from("#nav", {
-//     opacity: 0,
-//   });
-//   tl.from("#hero1 h1,#hero2 h1,#hero3 h2,#hero4 h1", {
-//     y: 140,
-//     stagger: 0.2,
-//   });
-//   tl.from(
-//     "#hero1, #page2",
-//     {
-//       opacity: 0,
-//     },
-//     "-=1.2"
-//   );
-// }
-
-// function cursorAnimation() {
-//   Shery.mouseFollower({
-//     skew: true,
-//     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-//     duration: 1,
-//   });
-//   Shery.makeMagnet("#nav-part2 h4");
-
-//   var videoContainer = document.querySelector("#video-container");
-//   var video = document.querySelector("#video-container video");
-//   videoContainer.addEventListener("mouseenter", function () {
-//     videoContainer.addEventListener("mousemove", function (dets) {
-//       gsap.to(".mousefollower", {
-//         opacity: 0,
-//       });
-//       gsap.to("#video-cursor", {
-//         left: dets.x - 570,
-//         y: dets.y - 300,
-//       });
-//     });
-//   });
-//   videoContainer.addEventListener("mouseleave", function () {
-//     gsap.to(".mousefollower", {
-//       opacity: 1,
-//     });
-//     gsap.to("#video-cursor", {
-//       left: "70%",
-//       top: "-15%",
-//     });
-//   });
-
-//   var flagVideo = 0;
-//   videoContainer.addEventListener("click", function () {
-//     if (flagVideo == 0) {
-//       video.play();
-//       video.style.opacity = 1;
-//       document.querySelector(
-//         "#video-cursor"
-//       ).innerHTML = `<i class="ri-pause-mini-fill"></i>`;
-//       gsap.to("#video-cursor", {
-//         scale: 0.5,
-//       });
-//       flagVideo = 1;
-//     } else {
-//       video.pause();
-//       video.style.opacity = 0;
-//       document.querySelector(
-//         "#video-cursor"
-//       ).innerHTML = `<i class="ri-play-mini-fill"></i>`;
-//       gsap.to("#video-cursor", {
-//         scale: 1,
-//       });
-//       flagVideo = 0;
-//     }
-//   });
-// }
-
-// function sheryAnimation() {
-//   Shery.imageEffect(".image-div", {
-//     style: 2,
-//     onUpdate() {
-//       document.body.style.overflow = "auto"; // Ensure scrolling is enabled
-//     },
-//     config: {
-//       resolutionXY: { value: 100 },
-//       distortion: { value: true },
-//       mode: { value: -10 },
-//       mousemove: { value: 3 },
-//       modeA: { value: 1 },
-//       modeN: { value: 3 },
-//       speed: { value: 1, range: [-500, 500], rangep: [-10, 10] },
-//       frequency: { value: 50, range: [-800, 800], rangep: [-50, 50] },
-//       angle: { value: 0.5, range: [0, 3.141592653589793] },
-//       waveFactor: { value: 1.4, range: [-3, 3] },
-//       color: { value: 10212607 },
-//       pixelStrength: { value: 3, range: [-20, 100], rangep: [-20, 20] },
-//       quality: { value: 5, range: [0, 10] },
-//       contrast: { value: 1, range: [-25, 25] },
-//       brightness: { value: 1, range: [-1, 25] },
-//       colorExposer: { value: 0.18, range: [-5, 5] },
-//       strength: { value: 0.2, range: [-40, 40], rangep: [-5, 5] },
-//       exposer: { value: 8, range: [-100, 100] },
-//       zindex: { value: -9996999, range: [-9999999, 9999999] },
-//       aspect: { value: 0.7666557722625823 },
-//       ignoreShapeAspect: { value: true },
-//       shapePosition: { value: { x: 0, y: 0 } },
-//       shapeScale: { value: { x: 0.5, y: 0.5 } },
-//       shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
-//       shapeRadius: { value: 0, range: [0, 2] },
-//       currentScroll: { value: 0 },
-//       scrollLerp: { value: 0.07 },
-//       gooey: { value: true },
-//       infiniteGooey: { value: false },
-//       growSize: { value: 4, range: [1, 15] },
-//       durationOut: { value: 1, range: [0.1, 5] },
-//       durationIn: { value: 1.5, range: [0.1, 5] },
-//       displaceAmount: { value: 0.5 },
-//       masker: { value: false },
-//       maskVal: { value: 1, range: [1, 5] },
-//       scrollType: { value: 0 },
-//       geoVertex: { range: [1, 64], value: 1 },
-//       noEffectGooey: { value: true },
-//       onMouse: { value: 0 },
-//       noise_speed: { value: 0.76, range: [0, 10] },
-//       metaball: { value: 0.6, range: [0, 2] },
-//       discard_threshold: { value: 0.5, range: [0, 1] },
-//       antialias_threshold: { value: 0, range: [0, 0.1] },
-//       noise_height: { value: 0.37, range: [0, 2] },
-//       noise_scale: { value: 7.63, range: [0, 100] },
-//       a: { value: 1.37, range: [0, 30] },
-//       b: { value: -0.91, range: [-1, 1] },
-//     },
-//     gooey: true,
-//   });
-// }
-
-// function flagAnimation() {
-//   document.addEventListener("mousemove", function (dets) {
-//     gsap.to("#flag", {
-//       x: dets.x,
-//       y: dets.y,
-//     });
-//   });
-
-//   document.querySelector("#hero3").addEventListener("mouseenter", function () {
-//     gsap.to("#flag", {
-//       opacity: 1,
-//     });
-//   });
-//   document.querySelector("#hero3").addEventListener("mouseleave", function () {
-//     gsap.to("#flag", {
-//       opacity: 0,
-//     });
-//   });
-// }
-
+locomotiveAnimation();
+loadingAnimation();
+cursorAnimation();
+flagAnimation();
 function initializeAnimations() {
   const isMobile =
-    /Android|webOS|iPhone|iPod|iPad|BlackBerry|IEMobile|Opera Mini|Chrome|Brave/i.test(
+    /Android|webOS|iPhone|iPod|iPad|BlackBerry|IEMobile|Opera Mini/i.test(
       navigator.userAgent
     );
-  console.log("Is mobile:", isMobile);
+  // console.log("Is mobile:", isMobile);
 
-  document.querySelector(".mobile").textContent = isMobile;
+  // document.querySelector(".mobile").textContent = isMobile;
 
   if (!isMobile) {
     console.log("Initializing animations for desktop");
-    locomotiveAnimation();
-    loadingAnimation();
-    cursorAnimation();
-    flagAnimation();
+    // cursorAnimation();
+    // flagAnimation();
     sheryAnimation();
   } else {
-    console.log("Initializing animations for mobile");
+    // console.log("Initializing animations for mobile");
     var extraImg = document.querySelectorAll(".extraImg");
     extraImg.forEach(function (img) {
       img.style.display = "none";
